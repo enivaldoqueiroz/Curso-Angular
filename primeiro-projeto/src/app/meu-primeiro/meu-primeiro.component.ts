53e2d01f4e23f1623559cf15e529bbf99b4e1a39
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-23 15:47:40 -0300

    Conhecendo o fluxo de criação de um componente no Angular

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+//Decoreitor Component
+@Component({
+    //Metadados
+    selector: 'meu-primeiro-component',
+    template: `<p>Meu primeiro component com Angular 11</p>`
+}) 
+
+export class MeuPrimeiroComponent{}
