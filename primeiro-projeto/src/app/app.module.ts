397473c7c5aac1cfef84ea38ab6d0ef43790e9dc
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-23 23:41:22 -0300

    Conhecendo os Módulos no Angular

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from "./meu-primeiro/meu-primeiro.component";
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
@@ -12,7 +13,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
