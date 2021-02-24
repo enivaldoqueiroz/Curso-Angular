9002cdfd27e96526487ea8fc4b5a082fee2c1b74
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-24 00:19:44 -0300

    Conhecendo a implementação do Template no Angular

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+    for (let index = 0; index < this.cursos.length; index++) {
+      let cursos = this.cursos[index];
+      
+    }
+   }
 
   ngOnInit(): void {
   }
