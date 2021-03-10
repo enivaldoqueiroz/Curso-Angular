7a28f280b96ba24904f441bb2107ee5fa2afc008
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-03-10 14:17:52 -0300

    Criação do component diretiva-ngif

---
+++ diretivas/src/app/diretivas-ngif/diretivas-ngif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-ngif',
+  templateUrl: './diretivas-ngif.component.html',
+  styleUrls: ['./diretivas-ngif.component.css']
+})
+export class DiretivasNgifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
