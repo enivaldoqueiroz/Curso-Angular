4ed6be42338c1e67884117cf86e4ee767daebaaf
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-05-07 12:14:34 -0300

    Diretivas: trabalhando com ngSwitch, ngSwitchCase e ngSwitchDefault

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiretivasNgifComponent } from './diretivas-ngif/diretivas-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivasNgifComponent
+    DiretivasNgifComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     BrowserModule
