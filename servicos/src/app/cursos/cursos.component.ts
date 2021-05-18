1acf51981f5e07c115bd047c49402f3362656c93
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-05-18 16:18:23 -0300

    Trabalhando com Serviço(Service) e criando o primeiro componente

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from "./cursos.service";
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  cursos: string[] = [];
+  CursosService: CursosService;
+
+  constructor() {
+    this.CursosService = new CursosService();
+   }
+
+  ngOnInit(): void {
+    this.cursos = this.CursosService.getCursos();
+  }
+
+}
