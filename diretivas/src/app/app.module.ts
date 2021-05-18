ffa75b4343ca78d4d56ed62b12acb72f65324523
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-05-18 15:48:27 -0300

    Trabalhando com diretiva de estrutura (ngElse)

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { HighlightDirective } from './shared/highlight.directive';
+import { NgElseDirective } from './shared/ng-else.directive';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,9 @@ import { HighlightDirective } from './shared/highlight.directive';
     FundoAmareloDirective,
     DiretivasCustomizadasComponent,
     HighlightMouseDirective,
-    HighlightDirective
+    HighlightDirective,
+    NgElseDirective,
+ 
   ],
   imports: [
     BrowserModule,
--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasCustomizadasComponent implements OnInit {
 
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMostraCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
